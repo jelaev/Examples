d5177c6bb0e500c280ba6727a7175dc16bea82f8
primax <primax2@bk.ru>
2020-06-03 17:54:51 +0300

    Добавьте файлы проекта.

---
+++ TodoConsole/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+using TodoLibrary;
+
+namespace TodoConsole
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            TodoList<TodoItem> MyList = new TodoList<TodoItem>();       
+            var handle = true;
+            while (handle)
+            {
+                Console.WriteLine($"Элементов в списке {MyList.Count}.");
+                Console.WriteLine($"1 - Добавить задание\t 2 - Выполнить задание\t 3 - Посмотреть задание");
+                Console.WriteLine($"4 - Удалить задание\t 5 - Все задания\t 6 - Выйти");
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        {
+                            Console.WriteLine("Введите текст задания: ");
+                            var z = Console.ReadLine();
+                            var todo = new TodoItem(MyList.Count, z, false);
+                            MyList.Add(todo);
+                            break;
+                        }
+                    case "2":
+                        {
+                            Console.WriteLine("Введите номер задания: ");
+                            var n = Console.ReadLine();
+                            MyList.Where(x => x.GetNumber().Equals(n)).FirstOrDefault().Checked = true;
+                            break;
+                        }
+                    case "3":
+                        {
+                            Console.WriteLine("Номер задания: ");
+                            var n = Console.ReadLine();
+                            Console.WriteLine(MyList.Where(x => x.GetNumber().Equals(x)).FirstOrDefault());
+                            break;
+                        }
+                    case "4":
+                        {
+                            Console.WriteLine("Номер задания для удаления: ");
+                            var n = Console.ReadLine();
+                            Console.Write("Удалено - " + MyList.RemoveAll(x => x.GetNumber().Equals(n)));
+                            break;
+                        }
+                    case "5":
+                        {
+                            MyList.ForEach(x => Console.WriteLine(x));
+                            break;
+                        }
+                    case "6":
+                        {
+                            handle = false;
+                            break;
+                        }
+                }
+
+            }
+            
+        }
+    }
+}
---
+++ TodoLibrary/TodoItem.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace TodoLibrary
+{
+    public class TodoItem
+    {
+        private object _number;
+        private object _task;
+        private object _checked;
+
+        public object Number { get => _number; set => _number = value; }
+        public object Task { get => _task; set => _task = value; }
+        public object Checked { get => _checked; set => _checked = value; }
+
+        public TodoItem(object n, object t, object c)
+        {
+            Number = n;
+            Task = t;
+            Checked = c;
+        }
+
+        public bool IsChecked()
+        {
+            return Convert.ToBoolean(Checked);
+        }
+
+        public string GetTask()
+        {
+            return Task.ToString();
+        }
+
+        public string GetNumber()
+        {
+            return Number.ToString();
+        }
+
+        public override string ToString()
+        {
+            return $"Номер: {GetNumber()}\t Выполнен: {IsChecked()}\t Задание: {GetTask()}";
+        }
+    }
+
+    public class TodoList<T> : List<T>
+    {
+        private List<T> _list;
+
+        public TodoList()
+        {
+            _list = new List<T>();
+        }
+
+       public void SetList(List<T> setList)
+        {
+            _list.AddRange(setList);
+        }
+    }
+}
